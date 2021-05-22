636eb3c890199f14a0b4cbf1fd0c7212e020fa2e
Stelmashchuk Oleh <Stelmashchuk.oleh@gmail.com>
2021-05-22 11:27:51 +0300

    fix re-render dep

--- frontend/out/_next/static/LsbF8Vp5SMmxoKxbAudg1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/690-a2f61ecf1265ec228dd6.js","static/css/06a8113e3ab6be54daf3.css","static/chunks/pages/index-074fba42bee774fbfa84.js"],"/_error":["static/chunks/pages/_error-88706918d4c44f607045.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
